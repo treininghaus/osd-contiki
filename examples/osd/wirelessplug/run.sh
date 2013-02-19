c5a9a22e0f94b2553762bad371e6317c4760f7dd
Harald Pichler <harald@the-develop.net>
2013-02-19 11:07:08 +0100

    change plattform to osd-merkur

--- examples/osd/wirelessplug/run.sh
+++ examples/osd/wirelessplug/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-make clean TARGET=osd-er-lp24
-make TARGET=osd-er-lp24
-avr-size -C --mcu=MCU=atmega128rfa1 er-example-server.osd-er-lp24
-avr-objcopy -j .text -j .data -O ihex er-example-server.osd-er-lp24 er-example-server.osd-er-lp24.hex
-avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex er-example-server.osd-er-lp24 er-example-server.osd-er-lp24.eep
-
+make clean TARGET=osd-merkur
+make TARGET=osd-merkur
+avr-size -C --mcu=MCU=atmega128rfa1 er-example-server.osd-merkur
+avr-objcopy -j .text -j .data -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.hex
+avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.eep
