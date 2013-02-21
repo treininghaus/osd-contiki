cb41c216e9e7b1fa43ba924032e5bbe849782975
Harald Pichler <harald@the-develop.net>
2013-02-21 16:00:48 +0100

    bugfix compile and flash

--- examples/osd/rpl-border-router/run.sh
+++ examples/osd/rpl-border-router/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 make clean TARGET=osd-merkur
 make TARGET=osd-merkur
-avr-size -C --mcu=MCU=atmega128rfa1 er-example-server.osd-merkur
-avr-objcopy -j .text -j .data -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.hex
-avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.eep
+avr-size -C --mcu=MCU=atmega128rfa1 border-router.osd-merkur
+avr-objcopy -j .text -j .data -O ihex border-router.osd-merkur border-router.osd-merkur.hex
+avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex border-router.osd-merkur border-router.osd-merkur.eep
