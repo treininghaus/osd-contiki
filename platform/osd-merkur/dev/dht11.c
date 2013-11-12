29c7297591edac4785e4f0be525441bce8f8ff67
Harald Pichler <harald@the-develop.net>
2013-11-12 13:27:13 +0100

    bugfix RH03 sensor

--- platform/osd-merkur/dev/dht11.c
+++ platform/osd-merkur/dev/dht11.c
@@ -46,22 +46,15 @@ uint8_t DHT_Read_Data(uint16_t *temperature, uint16_t *humidity){
     sreg = SREG;    /* Save status register before disabling interrupts. */
     cli();          /* Disable interrupts. */
 
-    //Prepare the bus
-    PIN_INIT();
     //Set pin Output
     //Pin High
     DHT_DRIVE();
-//    mdelay(250);                     //Wait for 250mS
     mdelay(100);                     //Wait for 100mS
     
     //Send Request Signal
     //Pin Low
     OUTP_0();                      //20ms Low 
-#if DHT11
-	mdelay(20);
-#else
-	udelay(500);
-#endif
+    mdelay(20);
     //Pin High
     OUTP_1();
     udelay(40);                      //40us High
@@ -74,7 +67,10 @@ uint8_t DHT_Read_Data(uint16_t *temperature, uint16_t *humidity){
     //Repeat for each Transistions
     for (i=0; i<MAXTIMINGS; i++) {
         //While state is the same count microseconds
-        while (laststate==DHT_INP()) {
+//   led1_on();
+//   led1_off();
+ 
+       while (laststate==DHT_INP()) {
             udelay(1);
             counter++;
             if (counter>254) break;
@@ -90,17 +86,14 @@ uint8_t DHT_Read_Data(uint16_t *temperature, uint16_t *humidity){
             //Shift data[] value 1 position left
             //Example. 01010100 if we shift it left one time it will be
             //10101000
- //   led1_on();
-
+ 
             data[j/8]<<=1;
             if (counter >= 15) {    //If it was high for more than 40uS
- //   led1_off();
+//   led1_on();
                 data[j/8]|=1;       //it means it is bit '1' so make a logic
- //   led1_on();
+//   led1_off();
             }                       //OR with the value (save it)
             j++;                    //making an OR by 1 to this value 10101000
- //   led1_off();
-
         }                           //we will have the resault 10101001
                                     //1 in 8-bit binary is 00000001
         //j/8 changes table record every 8 bits which means a byte has been saved
