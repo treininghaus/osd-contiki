4f0555790531e865f9c5a7277d992e58adc4fe8c
harald42 <harald@digiel-5.7>
2013-11-28 14:38:45 +0100

    remove old project use climate instead

--- examples/osd/er-rest-example-dht11/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-make clean TARGET=osd-merkur
-make TARGET=osd-merkur
-avr-size -C --mcu=MCU=atmega128rfa1 er-example-server.osd-merkur
-avr-objcopy -j .text -j .data -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.hex
-avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.eep
