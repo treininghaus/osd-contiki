67783600aa15031a239416a1bf300d19f288d659
harald <harald@debian.evva.com>
2014-03-11 16:28:54 +0100

    compile all osd-examples

---
+++ examples/osd/runall.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# er-rest-example-merkurboard
+echo "merkurboard"
+cd ./merkurboard
+./run.sh
+cd ..
+echo "done (merkurboard)"
+
+# er-rest-example-merkurboard
+echo "er-rest-example-merkurboard"
+cd ./er-rest-example-merkurboard
+./run.sh
+cd ..
+echo "done (er-rest-example-merkurboard)"
+
+# Climate
+echo "Climate"
+cd ./climate
+./run.sh
+cd ..
+echo "done (Climate)"
+
+# Embeddvm
+echo "Embedded-VM"
+cd ./embedd-vm-merkurboard
+./run.sh
+cd ..
+echo "done (Emedded-VM)"
+
+# Powerbox-4
+echo "Powerbox-4"
+cd ./powerbox
+./run.sh
+cd ..
+echo "done (Powerbox-4)"
+
+# Pingtheplug
+echo "Pingtheplug"
+cd ./pingtheplug
+./run.sh
+cd ..
+echo "done (Pingtheplug)"
+
+# 6lowpan-tk
+#echo "6loWPAN-Tür/Fenster Kontakt"
+#cd ./6lowpan-tk
+#./run.sh
+#cd ..
+#echo "done (6loWPAN-Tür/Fenster Kontakt)"
+
+# PIR-Sensor
+#echo "PIR-Sensor"
+#cd ./pir-sensor
+#./run.sh
+#cd ..
+#echo "done (PIR-Sensor)"
