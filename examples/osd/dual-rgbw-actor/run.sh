be01bf77a9b34520edea8c0678f8f8348f3b6dec
Ralf Schlatterbeck <rsc@runtux.com>
2014-05-13 16:56:59 +0200

    Allow changing bootloader_get_mac address
    Current default in the Makefile is the *new* bootloader address.
    But for backward compatibility we've modified the run*.sh files
    to use the old address. The run*.sh also now explain how to change
    the default.

--- examples/osd/dual-rgbw-actor/run.sh
+++ examples/osd/dual-rgbw-actor/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# For the new bootloader (using a jump-table) you want to use
+# BOOTLOADER_GET_MAC=0x0001ff80 (which is the current default)
 #make clean TARGET=osd-merkur
-make -j TARGET=osd-merkur
+make -j TARGET=osd-merkur BOOTLOADER_GET_MAC=0x0001f3a0
 #avr-size -C --mcu=MCU=atmega128rfa1 er-example-server.osd-merkur
 avr-objcopy -j .text -j .data -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.hex
 avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex er-example-server.osd-merkur er-example-server.osd-merkur.eep
